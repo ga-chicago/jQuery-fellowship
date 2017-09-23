b886d45b63f60e70219880de53d98e1a2550b0c1
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2017-09-23 12:35:16 -0500

    chap 9 complete - updated gandalf

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -295,12 +295,14 @@ forgeTheFellowShip()
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' textNode to 'Gandalf the White'
+  $(`.buddy:contains('Gandalf the Grey')`).text('Gandalf the White');
 
   // 2. add a class "the-white" to this element
 
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
 
 };
+theBalrog()
 
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 9 complete - Updated Gandalf"
@@ -311,6 +313,7 @@ const theBalrog = () => {
 const hornOfGondor = () => {
 
   // 1. create a pop-up alert that the horn of gondor has been blown
+
 
   // 2. Boromir's been killed by the Uruk-hai! Put a linethrough on Boromir's name
 
