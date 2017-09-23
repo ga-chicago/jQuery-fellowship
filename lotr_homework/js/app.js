6539fad74367c98543bf8fe0c9eeabb98eb519f5
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-09-22 19:31:40 -0500

    Chapter 9 complete - Updated Gandalf

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -239,10 +239,12 @@ const forgeTheFellowShip = () => {
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' textNode to 'Gandalf the White'
+    $(`.buddy:contains('Gandalf the Grey')`).text('Gandalf the White');
 
   // 2. add a class "the-white" to this element
-
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
+
+    $(`.buddy:contains('Gandalf the White')`).addClass('the-white')
 
 };
 
