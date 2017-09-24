d42f04e90ac3cdc0ac2d23b109df2f796aab9828
danielhkim92 <danielhkim92@gmail.com>
2017-09-24 14:45:52 -0500

    9

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -207,9 +207,9 @@ const forgeTheFellowShip = () => {
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' textNode to 'Gandalf the White'
-
+  const $gandalf = $( ".buddy" ).get( 0 );
   // 2. add a class "the-white" to this element
-
+  $($gandalf).addClass("the-white")
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
 
 };
