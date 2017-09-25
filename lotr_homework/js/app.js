6c7e53b5f5ae0e1cc2dbbd0d29560f00214aac34
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-09-24 23:28:03 -0500

    Chapter 9 complete updated Gandalf

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -156,17 +156,11 @@ const forgeTheFellowShip = () => {
 // Chapter 9
 // ============
 const theBalrog = () => {
-
-  // 1. change the 'Gandalf' textNode to 'Gandalf the White'
-
-  // 2. add a class "the-white" to this element
-
-  // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
-
+    $('.buddy').text(function () {
+    return $(this).text().replace("Grey", "White"); 
+});
 };
 
-// COMMIT YOUR WORK
-// The commit message should read: "Chapter 9 complete - Updated Gandalf"
 
 // ============
 // Chapter 10
